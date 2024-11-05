84288ac0fd723ab740217db78a98e0961553da1b
ArtAEurope <ingrid@askart.se>
2024-11-05 18:00:58 +0100

    Added colors. Placed them according to the layoutsheet.

--- xcodelaxa1/ContentView.swift
+++ xcodelaxa1/ContentView.swift
@@ -9,13 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        VStack (spacing: 5.0){
+            
+            HStack (spacing: 0.00) {
+                
+                Color("RedBox")
+                    .frame(maxWidth: .infinity,maxHeight:170)
+                
+                
+                Color("GreenBox")
+                    .frame(maxWidth: .infinity, maxHeight: 170)
+                
+                
+                Color("YellowBox")
+                    .frame(maxWidth: .infinity, maxHeight: 170)
+            }
+            
+            Color("GrayBox")
+                .frame(maxWidth: .infinity, maxHeight: 110)
+            
+            Color.white
+                .frame(maxWidth: 90, maxHeight: 90)
+        Spacer()
+            ZStack{
+                
+                
+                Color("RedBox")
+                    .frame(maxWidth: .infinity, maxHeight: 120)
+                HStack{
+                    Spacer()
+                    Color.black
+                        .frame(maxWidth:55,maxHeight:55)
+                        .padding(30)
+                    
+                }
+            }
+            
+                                
+    }
+        
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("Bluebackground"))
+                    
+                    .ignoresSafeArea()
+        
+        
+            
+        
+        
     }
 }
 
